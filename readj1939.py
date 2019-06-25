41c5b9117d99bb58a06a8d960037d15225472eec
Sam Hollifield <samuel.hollifield@gmail.com>
2019-06-24 21:54:16 -0500

    Update readj1939.py
    put message object in endless loop. Will end on ctrl+c

--- readj1939.py
+++ readj1939.py
@@ -3,5 +3,5 @@ import can
 from pprint import pprint
 db = cantools.database.load_file('j1939_20121116_TSC1_PTO_canalyser_11.dbc')
 bus = can.interface.Bus('can0', bustype='socketcan')
-message = bus.recv()
-db.decode_message(message.arbitration_id, message.data)
+for msg in bus:
+  db.decode_message(msg.arbitration_id, msg.data)

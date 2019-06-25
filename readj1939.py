e8f413adaeb7ae96ab15d808e071f2baf775ab46
Sam Hollifield <samuel.hollifield@gmail.com>
2019-06-25 06:48:54 -0500

    Update readj1939.py
    randomly produces keyerrors

--- readj1939.py
+++ readj1939.py
@@ -1,7 +1,11 @@
 import cantools
 import can
 from pprint import pprint
-db = cantools.database.load_file('j1939_20121116_TSC1_PTO_canalyser_11.dbc')
-bus = can.interface.Bus('can0', bustype='socketcan')
-for msg in bus:
-  db.decode_message(msg.arbitration_id, msg.data)
+db = cantools.database.load_file('j1939bus_181017_090023.dbc')
+bus = can.interface.Bus('vcan0', bustype='socketcan')
+canBR = can.BufferedReader()
+can.Notifier(bus, [canBR])
+
+while True:
+    message = canBR.get_message(timeout=.1)
+    db.decode_message(message.arbitration_id, message.data)
